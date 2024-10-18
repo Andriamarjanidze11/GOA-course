d52d830d721504ec156c20aa705194a1f2e5b79a
Andriamarjanidze11 <dudumarjanidze@gmail.com>
2024-10-18 20:36:50 +0400

    commit

---
+++ Level 03/class/level03.py
@@ -0,0 +1,18 @@
+name = 'Dudu'
+last_name = 'Marjanidze'
+age = 12
+country = 'Georgia'
+home = 'Saguramo'
+favourite_car = 'porsche 911'
+favourite_hobby = 'programming'
+favourite_color = 'red'
+
+print("hello I am " + name, last_name,"I am from " + country)
+
+
+
+
+
+
+
+
---
+++ Level 03/home/task3.py
@@ -0,0 +1,16 @@
+length = 20
+width = 10
+
+
+print(length * 2 + width * 2)
+
+
+
+
+
+
+
+
+
+
+
