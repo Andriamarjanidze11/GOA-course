f265ec7fdcd20894da514bb490e947179a3f6d34
Andriamarjanidze11 <dudumarjanidze@gmail.com>
2024-11-17 01:43:24 +0400

    commit

---
+++ Level 11/class/index.py
@@ -0,0 +1,61 @@
+#conditional statments/პირობითი განცხადებები
+
+#if/else statments
+
+
+age = 20
+
+if age > 18:
+    print("you can have a drivers license")
+else:
+    print("GET OUT!!")
+
+
+age = 19
+expirience = 3
+
+
+if age > 18 and expirience > 2:
+    print('you can take a drivers licence')
+else:
+    print('you cannot take a drivers licence')
+
+age = 25
+
+if age > 1 and age < 12:
+    print('child')
+elif age > 18 and age < 30:
+    print('teenager')
+elif age > 18 and age < 30:
+    print('adult')
+else:
+    print('senior')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Level 11/class/task1.py
@@ -0,0 +1,39 @@
+age = int(input())
+expirience = int(input())
+
+
+if int(age) > 18 and int(expirience) > 2:
+    print('you can take a drivers licence')
+else:
+    print('you cannot take a drivers licence')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
