d77878916a1241c844955ce391421179fbd6d810
Andriamarjanidze11 <dudumarjanidze@gmail.com>
2024-12-22 11:32:22 +0400

    commit

---
+++ Level 21/home/task2.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+print(min(fruits))
+print(max(fruits))
+
+fruits.append("fig")
+
+fruits.remove("banana")
+fruits[1] = "blueberry"
+
+x = len(fruits)
+
+print(x)
+
+print(fruits)
