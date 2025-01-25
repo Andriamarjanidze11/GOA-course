846d3e58b3e1b6c1080dede270575360ab3da6bb
Andriamarjanidze11 <dudumarjanidze@gmail.com>
2025-01-25 23:59:39 +0400

    commit

---
+++ Level 31/class/task1.py
@@ -0,0 +1,9 @@
+name = "Dudu"
+
+last_name = "Marjanidze"
+
+age = "13"
+
+sentance = f"Hello,my name is {name},my last name is {last_name} and i'm {age} years old"
+
+print(sentance)
---
+++ Level 31/home/task2.py
@@ -0,0 +1,5 @@
+alicewonderland = ['Alice','Wonderland']
+
+alicewonderland2 = 'Hello,{0}.Welcome to {1}.'.format(alicewonderland[0], alicewonderland[1])
+
+print(alicewonderland2)
