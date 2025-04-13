1bef2aa9725c238cd6c6bc91ff8f049678adb64b
Andriamarjanidze11 <dudumarjanidze@gmail.com>
2025-04-13 11:52:34 +0400

    commit

---
+++ Level 51/class/task1.py
@@ -0,0 +1,17 @@
+# The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+# To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+# Input
+
+def openOrSenior(data):
+    list =[]
+    for age, hcap in data:
+    
+        if age >= 55 and hcap > 7:
+            list.append('Senior')
+            
+        else: list.append('Open')
+        
+    return list
+    
---
+++ Level 51/class/task3.py
@@ -0,0 +1,5 @@
+# Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+def row_sum_odd_numbers(n):
+    #your code here
+    return n ** 3
